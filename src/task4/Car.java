c29bd23f3e8c717b15d85208451e0844b3ef8227
ushaswinir <ushaswinir.eorionitlabs@gmail.com>
2023-01-24 12:28:30 -0600

    adding task 4

---
+++ src/task4/Car.java
@@ -0,0 +1,35 @@
+package task4;
+
+public  abstract class Car {
+
+    private String model;
+    private String color;
+    private String make;
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMake() {
+        return make;
+    }
+
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public abstract String engineNumber(String make);
+}
+
---
+++ src/task4/mycar.java
@@ -0,0 +1,18 @@
+package task4;
+
+public class
+mycar {
+
+    public static void main(String[] args) {
+        String engineNumber;
+        BMW bmw = new BMW();
+        bmw.setModel("IX1");
+        bmw.setColor("Red");
+        bmw.setMake("2009");
+        engineNumber = bmw.engineNumber(bmw.getMake());
+        System.out.println("BMW");
+        System.out.println("Model : " + bmw.getModel() + ", Color : " + bmw.getColor() + ", Make : " + bmw.getMake() + ", EngineNumber : " + engineNumber);
+
+    }
+}
+
